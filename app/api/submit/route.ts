97212707feb1df9581ecc420a4887471c9b86803
kylemacabasco <kylemacabasco@gmail.com>
2025-07-15 04:16:14 -0700

    Implemented Supabase

---
+++ app/api/submit/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClerkSupabaseClient } from "@/app/lib/db";
+import { getAuth } from "@clerk/nextjs/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { slug, pdf } = body;
+  if (!slug || !pdf) {
+    return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+  }
+
+  // Get user ID from Clerk JWT
+  const { userId } = getAuth(req);
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  console.log(userId)
+  const supabase = await createClerkSupabaseClient(req);
+  const { data, error } = await supabase
+    .from("pdf_slugs")
+    .insert([{ slug, pdf, user_id: userId }])
+    .select();
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+  return NextResponse.json({ data }, { status: 200 });
+}
+          
---
+++ app/lib/db.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+import { NextRequest } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
+export async function createClerkSupabaseClient(req: NextRequest) {
+  return createClient(supabaseUrl, supabaseKey, {
+    async accessToken() {
+      const { getToken } = getAuth(req);
+      const token = await getToken();
+      console.log("token", token);
+      return token || '';
+    },
+  });
+} 
